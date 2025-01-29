a4b81f34be629751f11a657510384b7e8835bb09
daniel.andrade1991@gmail.com <daniel.andrade1991@gmail.com>
2025-01-29 16:48:02 -0300

    Commit inicial

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,20 @@
+import { Increment } from "../types";
+import { SorobanEvent } from "@subql/types-stellar";
+
+export async function handleEvent(event: SorobanEvent): Promise<void> {
+  if (event.transaction) {
+    logger.info(`Transaction hash: ${event.transaction.hash.toString()}`);
+    if (event.type.toString() == "contract") {
+      logger.info(`Event value: ${JSON.stringify(event.value)}`);
+      const increment = Increment.create({
+        id: event.transaction.hash,
+        newValue: BigInt(
+          JSON.parse(JSON.stringify(event.value))["_value"].toString()
+        ),
+      });
+      await increment.save();
+    }
+  } else {
+    logger.warn("The event does not have an associated transaction."); 
+  }
+}
